091b78b22ef9db29dd6d66dc367da4c9aaef0deb
Adrian07 <adrian.mirabal07@gmail.com>
2023-02-19 13:30:18 -0400

    Form

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -6,5 +6,27 @@ namespace WinFormsApp1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string textoPeso = txtPeso.Text;
+            double Peso = Double.Parse(textoPeso); //kg
+            string textoAltura = txtAltura.Text;
+            double Altura = Double.Parse(textoAltura); //m
+
+            double imc = Peso/ (Altura * Altura);
+
+            MessageBox.Show("El imc es: " + imc);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
